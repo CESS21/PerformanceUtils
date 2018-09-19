86cceb6fad1ffb79c44cd520dc44fa486befadec
Carter Hinsley <kvazau@protonmail.com>
2018-09-19 18:06:27 -0400

    Parameterize formula selection

--- src/Conversions.ts
+++ src/Conversions.ts
@@ -11,24 +11,40 @@ import * as T from "./lib/DataTypes";
 
 interface IFormula {
 
-    load(reps: T.Quantity, max: T.Load): T.Load;
+    load(
+        reps: T.Quantity,
+        max: T.Load
+        ): T.Load;
 
-    max(reps: T.Quantity, load: T.Load): T.Load;
+    max(
+        reps: T.Quantity,
+        load: T.Load
+        ): T.Load;
 
-    reps(intensity: T.Intensity): T.PartialQuantity;
+    reps(
+        intensity: T.Intensity
+        ): T.PartialQuantity;
 
 }
 
 
 class Brzycki {
 
-    public static load(reps: T.Quantity, max: T.Load): T.Load {
+    public static load(
+        reps: T.Quantity,
+        max: T.Load
+        ): T.Load
+    {
 
         return max * (37 - reps) / 36;
 
     }
 
-    public static max(reps: T.Quantity, load: T.Load): T.Load {
+    public static max(
+        reps: T.Quantity,
+        load: T.Load
+        ): T.Load
+    {
 
         return load * 36 / (37 - reps);
 
@@ -43,20 +59,28 @@ class Brzycki {
 }
 
 
+let default_formula : IFormula = Brzycki;
+
+
 // Calculate percentage intensity from supplied load used and one repetition
 // maximum.
-function intensity(load: T.Load, max: T.Load): T.Intensity {
+function intensity(
+    load: T.Load,
+    max: T.Load
+    ): T.Intensity
+{
     
     return load / max;
     
 }
 
 
-// Calculate maximal repetition quantity from a supplied percentage intensity
-// using Brzycki formula.
-function maxRepsFromIntensity(intensity: T.Intensity): T.Quantity {
-
-    let formula: IFormula = Brzycki;
+// Calculate maximal repetition quantity from a supplied percentage intensity.
+function maxRepsFromIntensity(
+    intensity: T.Intensity,
+    formula: IFormula = default_formula
+    ): T.Quantity
+{
 
     return Math.floor(formula.reps(intensity));
 
@@ -65,9 +89,12 @@ function maxRepsFromIntensity(intensity: T.Intensity): T.Quantity {
 
 // Calculate maximal repetition quantity from a supplied load to be used and one
 // repetition maximum load.
-function maxRepsFromLoads(load: T.Load, max: T.Load): T.Quantity {
-
-    let formula: IFormula = Brzycki;
+function maxRepsFromLoads(
+    load: T.Load,
+    max: T.Load,
+    formula: IFormula = default_formula
+    ): T.Quantity
+{
 
     return Math.floor(formula.reps(intensity(load, max)));
 
@@ -75,20 +102,27 @@ function maxRepsFromLoads(load: T.Load, max: T.Load): T.Quantity {
 
 
 // Calculate one-rep maximum weight from supplied load used and quantity of reps
-// performed, using Brzycki formula.
-function oneRepMax(reps: T.Quantity, load: T.Load): T.Load {
-
-    let formula: IFormula = Brzycki;
+// performed.
+function oneRepMax(
+    reps: T.Quantity,
+    load: T.Load,
+    formula: IFormula = default_formula
+    ): T.Load
+{
 
     return formula.max(reps, load);
 
 }
 
 
-// Calculate maximal weight that can be moved for supplied quantity of reps by a lifter with specified one rep max, using Brzycki formula.
-function repMax(reps: T.Quantity, max: T.Load): T.Load {
-
-    let formula: IFormula = Brzycki;
+// Calculate maximal weight that can be moved for supplied quantity of reps by a
+// lifter with specified one rep max.
+function repMax(
+    reps: T.Quantity,
+    max: T.Load,
+    formula: IFormula = default_formula
+    ): T.Load
+{
 
     return formula.load(reps, max);
 
