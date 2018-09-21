3d64edad1bee6811ac51f98033dcd8bcc2c40082
Carter Hinsley <kvazau@protonmail.com>
2018-09-20 20:20:14 -0400

    Add documentation for acronyms

--- datatypes.py
+++ datatypes.py
@@ -10,15 +10,21 @@
 # TODO: Write class for Percentage. Temporarily aliased to float.
 Percentage = float
 
+# Fatigue-Variability Product.
 FVP                   = float
+# Intensity * Number Of Lifts.
 INOL                  = float
 Intensity             = Percentage
 Load                  = float
 PartialQuantity       = float
 Quantity              = int
 RelativeVolume        = float
+# Relative Volume.
 RV                    = RelativeVolume
 RelativeVolumePercent = Percentage
+# Relative Volume: Percent.
 RVPercent             = RelativeVolumePercent
+# Repetition-Endurance Quotient.
 REQ                   = Percentage
+# Volume-Fatigue Index.
 VFI                   = float
